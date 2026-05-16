244b22f2e2e6407e429714c046b0d04782cedd01
강현 <hyeonwol12345@kakao.com>
2026-05-17 00:04:51 +0900

    Update install-with-limine.sh

--- install-with-limine.sh
+++ install-with-limine.sh
@@ -18,9 +18,9 @@ mkdir /mnt/var/{log,tmp,cache};
 mount -o noatime,commit=120,ssd,discard,space_cache=v2,compress=zstd,subvol=@home /dev/nvme0n1p2 /mnt/home;
 mount -o noatime,commit=120,ssd,discard,space_cache=v2,compress=zstd,subvol=@opt /dev/nvme0n1p2 /mnt/opt;
 mount -o noatime,commit=120,ssd,discard,space_cache=v2,compress=zstd,subvol=@srv /dev/nvme0n1p2 /mnt/srv;
-mount -o noatime,commit=120,ssd,discard,space_cache=v2,compress=zstd,subvol=@cache /dev/nvme0n1p3 /mnt/var/cache;
-mount -o noatime,commit=120,ssd,discard,space_cache=v2,compress=zstd,subvol=@log /dev/nvme0n1p3 /mnt/var/log;
-mount -o noatime,commit=120,ssd,discard,space_cache=v2,compress=zstd,subvol=@tmp /dev/nvme0n1p3 /mnt/var/tmp;
+mount -o noatime,commit=120,ssd,discard,space_cache=v2,compress=zstd,subvol=@cache /dev/nvme0n1p2 /mnt/var/cache;
+mount -o noatime,commit=120,ssd,discard,space_cache=v2,compress=zstd,subvol=@log /dev/nvme0n1p2 /mnt/var/log;
+mount -o noatime,commit=120,ssd,discard,space_cache=v2,compress=zstd,subvol=@tmp /dev/nvme0n1p2 /mnt/var/tmp;
 mount /dev/nvme0n1p1 /mnt/boot;
 echo ' ' > /mnt/etc/vconsole.conf
 pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-firmware amd-ucode dkms wireplumber pipewire-pulse pipewire-alsa pipewire-jack sof-firmware alsa-firmware networkmanager net-tools ntfs-3g exfat-utils btrfs-progs limine efibootmgr os-prober nano wget curl git;
